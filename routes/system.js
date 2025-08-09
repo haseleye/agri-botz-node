8c9e1b1823d8e90d40ddd12e6977168a07df1bdb
haseleye <haseleye@gmail.com>
2025-08-09 15:52:09 +0300

    Update system.js

--- routes/system.js
+++ routes/system.js
@@ -7,5 +7,8 @@ router.post('/update-person-images-cb', System.updatePersonImagesCB);
 router.post('/update-person-images-callback', System.updatePersonImagesCallback);
 
 router.post('/arduino-webhook', System.arduinoWebhook);
+router.all('/arduino-webhook', (req, res) => {
+    res.status(405).send(`Method Not Allowed: This endpoint only accepts POST requests.`);
+})
 
 module.exports = router;
