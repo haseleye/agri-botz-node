e51862f58df5da4751f96a0610c5bc60d47dee68
haseleye <haseleye@gmail.com>
2025-09-05 17:07:58 +0300

    Update iotCloud.js

--- routes/iotCloud.js
+++ routes/iotCloud.js
@@ -3,48 +3,48 @@ const router = express.Router();
 const iotCloud = require('../controllers/iotCloud')
 const {authorize} = require("../middleware/auth");
 
-router.post('/add-site', authorize('Api', ['User', 'Admin']), iotCloud.addSite);
+router.post('/add-site', authorize('Access', ['User', 'Admin']), iotCloud.addSite);
 
-router.post('/rename-site', authorize('Api', ['User', 'Admin']), iotCloud.renameSite);
+router.post('/rename-site', authorize('Access', ['User', 'Admin']), iotCloud.renameSite);
 
-router.post('/delete-site', authorize('Api', ['User', 'Admin']), iotCloud.deleteSite);
+router.post('/delete-site', authorize('Access', ['User', 'Admin']), iotCloud.deleteSite);
 
-router.post('/register-control-unit', authorize('Api', ['Admin']), iotCloud.registerControlUnit);
+router.post('/register-control-unit', authorize('Access', ['Admin']), iotCloud.registerControlUnit);
 
-router.post('/gear-control-unit', authorize('Api', ['Admin']), iotCloud.gearControlUnit);
+router.post('/gear-control-unit', authorize('Access', ['Admin']), iotCloud.gearControlUnit);
 
-router.post('/configure-control-unit', authorize('Api', ['User', 'Admin']), iotCloud.configureControlUnit);
+router.post('/configure-control-unit', authorize('Access', ['User', 'Admin']), iotCloud.configureControlUnit);
 
-router.post('/add-device', authorize('Api', ['Admin']), iotCloud.addDevice);
+router.post('/add-device', authorize('Access', ['Admin']), iotCloud.addDevice);
 
-router.post('/update-gadget-gps', authorize('Api', ['User']), iotCloud.updateGadgetGPS);
+router.post('/update-gadget-gps', authorize('Access', ['User']), iotCloud.updateGadgetGPS);
 
-router.post('/add-gadget', authorize('Api', ['Admin']), iotCloud.addGadget);
+router.post('/add-gadget', authorize('Access', ['Admin']), iotCloud.addGadget);
 
-router.post('/rename-gadget', authorize('Api', ['User']), iotCloud.renameGadget);
+router.post('/rename-gadget', authorize('Access', ['User']), iotCloud.renameGadget);
 
-router.post('/add-variable', authorize('Api', ['Admin']), iotCloud.addVariable);
+router.post('/add-variable', authorize('Access', ['Admin']), iotCloud.addVariable);
 
-router.post('/update-variable', authorize('Api', ['User']), iotCloud.updateVariable);
+router.post('/update-variable', authorize('Access', ['User']), iotCloud.updateVariable);
 
-router.post('/activate-site', authorize('Api', ['Admin']), iotCloud.activateSite);
+router.post('/activate-site', authorize('Access', ['Admin']), iotCloud.activateSite);
 
-router.post('/deactivate-site', authorize('Api', ['Admin']), iotCloud.deactivateSite);
+router.post('/deactivate-site', authorize('Access', ['Admin']), iotCloud.deactivateSite);
 
-router.post('/terminate-site', authorize('Api', ['Admin']), iotCloud.terminateSite);
+router.post('/terminate-site', authorize('Access', ['Admin']), iotCloud.terminateSite);
 
-router.post('/activate-device', authorize('Api', ['Admin']), iotCloud.activateDevice);
+router.post('/activate-device', authorize('Access', ['Admin']), iotCloud.activateDevice);
 
-router.post('/deactivate-device', authorize('Api', ['Admin']), iotCloud.deactivateDevice);
+router.post('/deactivate-device', authorize('Access', ['Admin']), iotCloud.deactivateDevice);
 
-router.post('/terminate-device', authorize('Api', ['Admin']), iotCloud.terminateDevice);
+router.post('/terminate-device', authorize('Access', ['Admin']), iotCloud.terminateDevice);
 
 router.post('/get-user-sites', authorize('Access', ['User', 'Admin']), iotCloud.getUserSites);
 
-router.post('/get-site-info', authorize('Api', ['User', 'Admin']), iotCloud.getSiteInfo);
+router.post('/get-site-info', authorize('Access', ['User', 'Admin']), iotCloud.getSiteInfo);
 
-router.post('/get-gadget-info', authorize('Api', ['User', 'Admin']), iotCloud.getGadgetInfo);
+router.post('/get-gadget-info', authorize('Access', ['User', 'Admin']), iotCloud.getGadgetInfo);
 
-router.post('/get-device-info', authorize('Api', ['Admin']), iotCloud.getDeviceInfo);
+router.post('/get-device-info', authorize('Access', ['Admin']), iotCloud.getDeviceInfo);
 
 module.exports = router;
